2e84e531b5b16bfe18a59654d499468e29a94da1
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2021-08-26 21:19:38 -0500

    Creación proyecto en VS

---
+++ Maze/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Medir el tiempo que tarda en encontrar el path
+
+        }
+    }
+}
